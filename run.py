5747381d617b93216921d3f8e1d8eca14b328ffa
annalisa-f <annalisa.fleming6@gmail.com>
2020-08-26 18:20:46 +1200

    PROGRESS

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, g
-
 import sqlite3
 
 app = Flask(__name__)
+RECIPEDB = 'recipes.db'
 
 @app.route('/')
 def index():
@@ -30,4 +30,52 @@ def add():
 
 @app.route('/confirm', methods=['POST'])
 def confirm():
-    return render_template('confirm.html')
+
+    name = 'name'
+    ingredients = []
+    quantity = []
+    method = []
+
+    for input in request.form:
+        if input == 'name':
+            name = request.form[input]
+        elif 'quantity' in input:
+            quantity.append(request.form[input])
+        elif 'ingredient' in input:
+            ingredients.append(request.form[input])
+        elif 'method' in input:
+            method.append(request.form[input])
+
+    print(name, quantity, ingredients, method)
+    ingreds = dict(zip(ingredients, quantity))
+
+    db = sqlite3.connect(RECIPEDB)
+
+    cur= db.execute(
+        'INSERT INTO recipes(name, ingredients, method) VALUES(?, ?, ?)',
+        (name, str(ingreds), str(method))
+    )
+
+    db.commit()
+    db.close()
+
+
+    return render_template('confirm.html', name=name, ingredients=ingreds, quantity=quantity, method=method)
+
+@app.route('/view')
+def view():
+
+    db = sqlite3.connect(RECIPEDB)
+
+    id = []
+    name = []
+    ingredients = {}
+    method = []
+
+    cur= db.execute(
+        'SELECT id, name, ingredients, method FROM recipes'
+    )
+
+    db.close()
+
+    return render_template('view.html', id=id)
